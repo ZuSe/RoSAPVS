3ef37ed0b9f92c5f0abb1abefe6e835dd7814726
patrickp <b4str4@web.de>
2014-06-16 12:57:41 +0200

    Push structure for dome

---
+++ ZY_SS14_T01_RoSAPVS/WebContent/zy_ss14_t01_rosapvs/Landing.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("zy_ss14_t01_rosapvs.Landing", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf zy_ss14_t01_rosapvs.Landing
+	*/ 
+	getControllerName : function() {
+		return "zy_ss14_t01_rosapvs.Landing";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf zy_ss14_t01_rosapvs.Landing
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
